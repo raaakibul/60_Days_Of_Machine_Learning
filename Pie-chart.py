0bed911e650e693373c826dbff85d0f9427d0858
rakibul-islam <raakibul.uap@gmail.com>
2023-11-29 21:19:47 +0600

    pie chart data visualization code added

--- Pie-chart.py
+++ Pie-chart.py
@@ -11,3 +11,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+pie_plot = np.array([20,25,15,20,20])
+pie_labels = ["Bananas", "Apples","Mangoes","Dates","Limes"]
+pie_explodes = [0.2,0,0,0,0]
+
+plt.pie(pie_plot, labels=pie_labels, explode=pie_explodes)
+plt.legend()
+plt.show()
