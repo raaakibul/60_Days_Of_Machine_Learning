50c20eef3180d09cd6b9fc0b459ecb23e0090c69
rakibul-islam <raakibul.uap@gmail.com>
2023-11-29 21:13:11 +0600

    first commit

---
+++ Pie-chart.py
@@ -0,0 +1,13 @@
+# Data Visualization with Matplotlib
+
+# Pie Charts
+
+# Lets create a pie chart👇
+
+# With Pyplot, you can use the pie() function to draw pie charts.
+
+# A simple pie chart:
+
+import matplotlib.pyplot as plt
+import numpy as np
+
